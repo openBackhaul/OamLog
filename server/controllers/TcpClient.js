3a0c8fe382db0908481b0655893c75978a4330ac
Dana Sunalova <dana@paxet.io>
2023-03-22 09:32:53 +0100

    TcpClient service should notify Elasticsearch
    Signed-off-by: Dana Sunalova <dana@paxet.io>

--- server/controllers/TcpClient.js
+++ server/controllers/TcpClient.js
@@ -5,11 +5,9 @@ var responseBuilder = require('onf-core-model-ap/applicationPattern/rest/server/
 var responseCodeEnum = require('onf-core-model-ap/applicationPattern/rest/server/ResponseCode');
 var oamLogService = require('onf-core-model-ap/applicationPattern/services/OamLogService');
 
-
-
-module.exports.getTcpClientRemotePort = async function getTcpClientRemotePort(req, res, next, uuid) {
+module.exports.getTcpClientRemotePort = function getTcpClientRemotePort(req, res, next, uuid) {
   let responseCode = responseCodeEnum.code.OK;
-  await TcpClient.getTcpClientRemotePort(req.url)
+  TcpClient.getTcpClientRemotePort(req.url)
     .then(function (response) {
       responseBuilder.buildResponse(res, responseCode, response);
     })
@@ -21,9 +19,9 @@ module.exports.getTcpClientRemotePort = async function getTcpClientRemotePort(re
 };
 
 
-module.exports.putTcpClientRemotePort = async function putTcpClientRemotePort(req, res, next, body, uuid) {
+module.exports.putTcpClientRemotePort = function putTcpClientRemotePort(req, res, next, body, uuid) {
   let responseCode = responseCodeEnum.code.NO_CONTENT;
-  await TcpClient.putTcpClientRemotePort(req.url, body,uuid)
+  TcpClient.putTcpClientRemotePort(body, uuid)
     .then(function (response) {
       responseBuilder.buildResponse(res, responseCode, response);
     })
@@ -35,9 +33,9 @@ module.exports.putTcpClientRemotePort = async function putTcpClientRemotePort(re
 };
 
 
-module.exports.getTcpClientRemoteAddress = async function getTcpClientRemoteAddress(req, res, next, uuid) {
+module.exports.getTcpClientRemoteAddress = function getTcpClientRemoteAddress(req, res, next, uuid) {
   let responseCode = responseCodeEnum.code.OK;
-  await TcpClient.getTcpClientRemoteAddress(req.url)
+  TcpClient.getTcpClientRemoteAddress(req.url)
     .then(function (response) {
       responseBuilder.buildResponse(res, responseCode, response);
     })
@@ -49,9 +47,9 @@ module.exports.getTcpClientRemoteAddress = async function getTcpClientRemoteAddr
 };
 
 
-module.exports.getTcpClientRemoteProtocol = async function getTcpClientRemoteProtocol(req, res, next, uuid) {
+module.exports.getTcpClientRemoteProtocol = function getTcpClientRemoteProtocol(req, res, next, uuid) {
   let responseCode = responseCodeEnum.code.OK;
-  await TcpClient.getTcpClientRemoteProtocol(req.url)
+  TcpClient.getTcpClientRemoteProtocol(req.url)
     .then(function (response) {
       responseBuilder.buildResponse(res, responseCode, response);
     })
@@ -62,29 +60,28 @@ module.exports.getTcpClientRemoteProtocol = async function getTcpClientRemotePro
   oamLogService.recordOamRequest(req.url, req.body, responseCode, req.headers.authorization, req.method);
 };
 
-module.exports.putTcpClientRemoteAddress = function putTcpClientRemoteAddress (req, res, next, body, uuid) {
-  let responseCode = responseCodeEnum.code.NO_CONTENT; 
-  TcpClient.putTcpClientRemoteAddress(req.url,body,uuid)
-  .then(function (response) {
-    console.log(response)
-    responseBuilder.buildResponse(res, responseCode, response);
-  })
-  .catch(function (response) {
-    responseCode = responseCodeEnum.code.INTERNAL_SERVER_ERROR;
-    responseBuilder.buildResponse(res, responseCode, response);
-  });
-oamLogService.recordOamRequest(req.url, req.body, responseCode, req.headers.authorization, req.method);
+module.exports.putTcpClientRemoteAddress = function putTcpClientRemoteAddress(req, res, next, body, uuid) {
+  let responseCode = responseCodeEnum.code.NO_CONTENT;
+  TcpClient.putTcpClientRemoteAddress(body, uuid)
+    .then(function (response) {
+      responseBuilder.buildResponse(res, responseCode, response);
+    })
+    .catch(function (response) {
+      responseCode = responseCodeEnum.code.INTERNAL_SERVER_ERROR;
+      responseBuilder.buildResponse(res, responseCode, response);
+    });
+  oamLogService.recordOamRequest(req.url, req.body, responseCode, req.headers.authorization, req.method);
 };
 
-module.exports.putTcpClientRemoteProtocol = function putTcpClientRemoteProtocol (req, res, next, body, uuid) {
-  let responseCode = responseCodeEnum.code.NO_CONTENT; 
-  TcpClient.putTcpClientRemoteProtocol(req.url,body,uuid)
-  .then(function (response) {
-    responseBuilder.buildResponse(res, responseCode, response);
-  })
-  .catch(function (response) {
-    responseCode = responseCodeEnum.code.INTERNAL_SERVER_ERROR;
-    responseBuilder.buildResponse(res, responseCode, response);
-  });
-oamLogService.recordOamRequest(req.url, req.body, responseCode, req.headers.authorization, req.method);
+module.exports.putTcpClientRemoteProtocol = function putTcpClientRemoteProtocol(req, res, next, body, uuid) {
+  let responseCode = responseCodeEnum.code.NO_CONTENT;
+  TcpClient.putTcpClientRemoteProtocol(body, uuid)
+    .then(function (response) {
+      responseBuilder.buildResponse(res, responseCode, response);
+    })
+    .catch(function (response) {
+      responseCode = responseCodeEnum.code.INTERNAL_SERVER_ERROR;
+      responseBuilder.buildResponse(res, responseCode, response);
+    });
+  oamLogService.recordOamRequest(req.url, req.body, responseCode, req.headers.authorization, req.method);
 };
